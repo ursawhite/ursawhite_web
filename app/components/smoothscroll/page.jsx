af415f95642d132d6155c00635fecd2f85a345bc
NLS <naufalls21@gmail.com>
2023-09-13 15:14:15 +0700

    test

--- app/components/smoothscroll/page.jsx
+++ app/components/smoothscroll/page.jsx
@@ -1,21 +1,21 @@
-"use client";
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+// "use client";
+// import { useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
 
-export default function SmoothScroll({ children }) {
-  console.log(children);
-  useEffect(() => {
-    window.scrollTo(0, 0);
+// export default function SmoothScroll({ children }) {
+//   console.log(children);
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
 
-    const lenis = new Lenis();
+//     const lenis = new Lenis();
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+//     function raf(time) {
+//       lenis.raf(time);
+//       requestAnimationFrame(raf);
+//     }
 
-    requestAnimationFrame(raf);
-  }, []);
+//     requestAnimationFrame(raf);
+//   }, []);
 
-  return children;
-}
+//   return children;
+// }
