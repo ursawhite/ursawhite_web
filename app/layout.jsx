af03578a9014b1714a65e62b46a9b481dfaf86d8
NLS <naufalls21@gmail.com>
2023-10-05 15:38:16 +0700

    [test]

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,8 +8,7 @@ export default function RootLayout({ children }) {
       <head>
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-          referrerPolicy="no-referrer"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"
           as="style"
         />
       </head>
@@ -25,8 +24,7 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
 
         <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-          strategy="afterInteractive"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
           as="script"
         />
       </body>
