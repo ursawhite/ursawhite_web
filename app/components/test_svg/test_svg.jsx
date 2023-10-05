7c9e63f86f7be0d68c141608cc87d317188819a0
NLS <naufalls21@gmail.com>
2023-10-06 00:12:32 +0700

    [test]

--- app/components/test_svg/test_svg.jsx
+++ app/components/test_svg/test_svg.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import "./test_svg.css";
+import styles from "./test_svg.module.css";
 
 function Test_svg({ item }) {
   const text = item;
   return (
-    <div className="patterns" style={{ marginTop: "-25%", zIndex: -10 }}>
+    <div className={styles.patterns}>
       <svg width="100%" height="800px">
         <text
+          className={styles.text}
           x="50%"
           y="50%"
           textAnchor="middle"
