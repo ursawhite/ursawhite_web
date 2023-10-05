30ee89a0a0432dd1ab33140d97e2f68683b538c2
NLS <naufalls21@gmail.com>
2023-10-06 02:46:08 +0700

    [add] manifest

---
+++ app/manifest.js
@@ -0,0 +1,31 @@
+export default function manifest() {
+  return {
+    name: "Ursawhite Web",
+    short_name: "Ursawhite Web",
+    description: "Ursawhite Web",
+    start_url: "/",
+    display: "standalone",
+    icons: [
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
