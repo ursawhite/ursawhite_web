5a354856cf2e852ec9e85161f35f159801339e26
NLS <naufalls21@gmail.com>
2023-10-07 12:34:40 +0700

    [test]

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,38 @@ import "bootstrap/dist/css/bootstrap.css";
 import Script from "next/script";
 import Navbar from "./components/header/navbar";
 
+export const metadata = {
+  metadataBase: new URL("https://ursawhite-web.vercel.app/"),
+  title: "Ursawhite Pte Ltd | Home",
+  description: "Home Page of Ursawhite Pte Ltd",
+  openGraph: {
+    title: "Ursawhite Pte Ltd | Home",
+    description: "Home Page of Ursawhite Pte Ltd",
+    url: "https://ursawhite-web.vercel.app/",
+    siteName: "Ursawhite Pte Ltd | Home",
+    images: "/opengraph-image.jpg",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ursawhite Pte Ltd | Home",
+    images: "/opengraph-image.jpg",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,5 @@
 import Home from "./home/home_page.jsx";
 
-export const metadata = {
-  title: "Ursawhite Pte Ltd | Home",
-  description: "Home Page of Ursawhite Pte Ltd",
-  openGraph: {
-    title: "Ursawhite Pte Ltd | Home",
-    description: "Home Page of Ursawhite Pte Ltd",
-    url: "https://ursawhite-web.vercel.app/",
-    siteName: "Ursawhite Pte Ltd | Home",
-    images: "/opengraph-image.jpg",
-    locale: "en_US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Ursawhite Pte Ltd | Home",
-    images: "/opengraph-image.jpg",
-  },
-};
-
 function HomePage() {
   return (
     <>
